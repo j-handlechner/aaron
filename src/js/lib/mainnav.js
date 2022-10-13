adc41b681acb0f9c901b96ba9246ce8af50779c7
Johannes Handlechner <jhandlechner.mmt-b2021@fh-salzburg.ac.at>
2022-10-13 22:03:21 +0200

    add header , workshops, about + nav/hamburger design

---
+++ src/js/lib/mainnav.js
@@ -0,0 +1,21 @@
+export function init() {
+    const btn = document.getElementById("hamburger")
+
+    btn.addEventListener("click", (e) => {
+        if(btn.ariaLabel == "closed") {
+            btn.ariaLabel = "opened";
+        }
+
+        else {
+            btn.ariaLabel = "closed";
+        }
+    })
+
+    // for closing the expaneded nav when clicking on a link
+    const navElements = document.querySelectorAll("#hamburger + ul li a")
+    for(let el of navElements) {
+        el.addEventListener("click", (e) => {
+            btn.ariaLabel = "closed";
+        })
+    }
+}
